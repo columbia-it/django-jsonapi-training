a2575fec96fb0903444e60931de9b84429640c46
Alan Crosswell <alan@columbia.edu>
2018-10-27 16:46:57 -0400

    removed regex from path

--- training/urls.py
+++ training/urls.py
@@ -65,5 +65,5 @@ urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
