8930f48ab6bdeee2ac604c30b08b2e425146256c
Alan Crosswell <alan@columbia.edu>
2018-10-27 16:48:02 -0400

    views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework_json_api.views import ModelViewSet, RelationshipView
 
-# Create your views here.
+from myapp.models import Course, CourseTerm
+from myapp.serializers import CourseSerializer, CourseTermSerializer
+
+class CourseViewSet(ModelViewSet):
+    """
+    API endpoint that allows course to be viewed or edited.
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseTermViewSet(ModelViewSet):
+    """
+    API endpoint that allows CourseTerm to be viewed or edited.
+    """
+    queryset = CourseTerm.objects.all()
+    serializer_class = CourseTermSerializer
+
+
+class CourseRelationshipView(RelationshipView):
+    """
+    view for relationships.course
+    """
+    queryset = Course.objects
+    self_link_view_name = 'course-relationships'
+
+
+class CourseTermRelationshipView(RelationshipView):
+    """
+    view for relationships.course_terms
+    """
+    queryset = CourseTerm.objects
+    self_link_view_name = 'course_term-relationships'
