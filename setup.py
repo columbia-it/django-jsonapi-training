d8df8fb5f90307f8495fbc0f0d605a21f56d5927
Alan Crosswell <alan@columbia.edu>
2024-07-10 16:02:03 -0400

    fix setup.py to eliminate duplicates

--- setup.py
+++ setup.py
@@ -1,7 +1,36 @@
 import os
+import re
+
 from setuptools import find_packages, setup
 
-from myapp import VERSION
+def get_version():
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join('myapp', '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
+                     init_py, re.MULTILINE).group(1)
+
+version=get_version()
+
+def parse_requirements(filename):
+    """Read requirements file and include recursive requirements."""
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('-r '):
+                # Recursively read included requirements files
+                included_file = line.split(' ')[1]
+                if os.path.exists(included_file):
+                    lines.extend(parse_requirements(included_file))
+            elif line and not line.startswith('#'):
+                lines.append(line)
+    return lines
+
+# Parse the requirements.txt file
+requirements = parse_requirements('requirements.txt')
+print(f"requirements: {requirements}")
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding="utf-8") as readme:
     README = readme.read()
@@ -11,7 +40,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='myapp',
-    version=VERSION,
+    version=version,
     packages=find_packages(),
     include_package_data=True,
     zip_safe = False,
@@ -22,9 +51,5 @@ setup(
     download_url='https://gitlab.cc.columbia.edu/ac45/django-training/repository/master/archive.tar.gz',
     author='Alan Crosswell',
     author_email='alan@columbia.edu',
-    # TODO: this duplicates requirements.txt
-    install_requires=[
-        'Django>=5.0',
-    #    'django-admin',
-    ]
+    install_requires=requirements,
 )
