f32c96d0db4ffcc3a10efba56307cc6465b2cd74
Alan Crosswell <alan@columbia.edu>
2024-07-31 16:35:49 -0400

    add some OAuth2 securitySchemes

--- myapp/schema.py
+++ myapp/schema.py
@@ -3,13 +3,88 @@ from pprint import pprint
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from drf_spectacular.plumbing import build_bearer_security_scheme_object
 
-class OAuth2AccessTokenAuthenticationScheme(OpenApiAuthenticationExtension):
-    target_class = 'oauth2_provider.contrib.rest_framework.OAuth2Authentication'
-    name = 'OAuth2'
+class MyAuthenticationSchemes(OpenApiAuthenticationExtension):
+    """
+    Make a list of securityScheme names and return a parallel list of definitions
+    """
+    target_class = "oauth2_provider.contrib.rest_framework.OAuth2Authentication"
+    name = ["oauth2-test", "oauth2-prod", "oauth2-local"]
+
 
     def get_security_definition(self, auto_schema):
         # TODO -- flesh out Django-oauth-toolkit stuff
-        r = build_bearer_security_scheme_object(header_name='AUTHORIZATION',
-                                                   token_prefix='Bearer',)
-        pprint(r)
-        return r
+        # r = build_bearer_security_scheme_object(header_name="AUTHORIZATION",
+        #                                         token_prefix="Bearer",)
+        return [
+            {
+                "type": "oauth2",
+                "description": "test OAuth2 service",
+                "flows": {
+                    "authorizationCode": {
+                        "authorizationUrl": "https://oauth-test.cc.columbia.edu/as/authorization.oauth2",
+                        "tokenUrl": "https://oauth-test.cc.columbia.edu/as/token.oauth2",
+                        "scopes": {
+                            "auth-columbia": "Columbia UNI login",
+                            "create": "create",
+                            "read": "read",
+                            "update": "update",
+                            "delete": "delete",
+                            "openid": "disclose your identity",
+                            "profile": "your user profile",
+                            "email": "your email address",
+                            "https://api.columbia.edu/scope/group": "groups you are a member of",
+                            "demo-djt-sla-bronze": "permitted to access the django-jsonapi-training demo: 1 request per second",
+                            "demo-djt-sla-update": "permitted to update the django-jsonapi-training resources",
+                        }
+                    }
+                }
+            },
+            {
+                "type": "oauth2",
+                "description": "OAuth2 service",
+                "flows": {
+                    "authorizationCode": {
+                        "authorizationUrl": "https://oauth.cc.columbia.edu/as/authorization.oauth2",
+                        "tokenUrl": "https://oauth.cc.columbia.edu/as/token.oauth2",
+                        "scopes": {
+                            "auth-columbia": "Columbia UNI login",
+                            "create": "create",
+                            "read": "read",
+                            "update": "update",
+                            "delete": "delete",
+                            "openid": "disclose your identity",
+                            "profile": "your user profile",
+                            "email": "your email address",
+                            "https://api.columbia.edu/scope/group": "groups you are a member of",
+                            "demo-djt-sla-bronze": "permitted to access the django-jsonapi-training demo: 1 request per second",
+                            "demo-djt-sla-update": "permitted to update the django-jsonapi-training resources",
+                        }
+                    }
+                }
+            },
+            {
+                "type": "oauth2",
+                "description": "local DOT OAuth2 service",
+                "flows": {
+                    "authorizationCode": {
+                        "authorizationUrl": "http://localhost:8000/o/authorize/",
+                        "tokenUrl": "http://localhost:8000/o/token/",
+                        "scopes": {
+                            "auth-columbia": "Columbia UNI login",
+                            "create": "create",
+                            "read": "read",
+                            "update": "update",
+                            "delete": "delete",
+                            "openid": "disclose your identity",
+                            "profile": "your user profile",
+                            "email": "your email address",
+                            "https://api.columbia.edu/scope/group": "groups you are a member of",
+                            "demo-djt-sla-bronze":
+                                "permitted to access the django-jsonapi-training demo: 1 request per second",
+                            "demo-djt-sla-update":
+                                "permitted to update the django-jsonapi-training resources"
+                        }
+                    }
+                }
+            },
+        ]
