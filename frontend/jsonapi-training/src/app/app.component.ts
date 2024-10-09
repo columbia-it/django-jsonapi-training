5fc58584cbdcc04dc4e22af51af1a6bf40177331
Alan Crosswell <alan@columbia.edu>
2024-10-09 16:03:12 -0400

    some initial customizations

--- frontend/jsonapi-training/src/app/app.component.ts
+++ frontend/jsonapi-training/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CourseComponent} from './course/course.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/jsonapi-training/src/app/course/course.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  standalone: true,
+  imports: [],
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css'],
+})
+export class CourseComponent {
+  title = 'course';
+
+}
