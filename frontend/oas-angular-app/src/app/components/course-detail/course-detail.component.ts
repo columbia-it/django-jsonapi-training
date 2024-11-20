5ed97f282f6a4e01f3a5020bf6a033b4fc092fd1
Alan Crosswell <alan@columbia.edu>
2024-11-20 14:53:13 -0500

    look up instructors/people for course_term

--- frontend/oas-angular-app/src/app/components/course-detail/course-detail.component.ts
+++ frontend/oas-angular-app/src/app/components/course-detail/course-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CoursesService, Course } from '../../core/api/v1';
+import { CoursesService, InstructorsService } from '../../core/api/v1';
 
 @Component({
   selector: 'app-course-detail',
@@ -9,10 +9,12 @@ import { CoursesService, Course } from '../../core/api/v1';
 })
 export class CourseDetailComponent implements OnInit {
   course: any;
+  instructors: { [key: string]: any } = {};
 
   constructor(
     private route: ActivatedRoute,
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
+    private instructorsService: InstructorsService
   ) {}
 
   ngOnInit() {
@@ -23,9 +25,29 @@ export class CourseDetailComponent implements OnInit {
 	(course) => {
 	  this.course = course;
 	  console.log(this.course)
+    this.loadInstructors();
 	},
 	(error) => console.error('Error:', error)
       );
     }
+  }
+
+  loadInstructors() {
+    if (!this.course?.included) return;
+    const instructorIds = new Set(
+      this.course.included
+        .filter((item: any) => item.type === 'course_terms')  // Filter for course_terms
+        .flatMap((term: any) => term.relationships.instructors.data.map((inst: any) => inst.id))
+    );
+
+    instructorIds.forEach((id) => {
+      const instructorId = id as string;
+
+      this.instructorsService.instructorsRetrieve(instructorId, [], ['person']).subscribe(
+        (instructor) => (this.instructors[instructorId] = instructor),
+        (error) => console.error(`Error fetching instructor ${id}:`, error)
+      );
+    })
+    console.log(this.instructors);
   }
 }
