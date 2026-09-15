3f53af810ea864851bda3549e04db1e506d63efd
Alan Crosswell <alan@columbia.edu>
2026-09-15 09:05:58 -0400

    remove frontend due to extensive dependency issues.

--- frontend/oas-angular-app/src/app/components/confirm-logout-dialog/confirm-logout-dialog.component.ts
+++
@@ -1,9 +0,0 @@
-// confirm-logout-dialog.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-confirm-logout-dialog',
-    templateUrl: './confirm-logout-dialog.component.html',
-    standalone: false
-})
-export class ConfirmLogoutDialogComponent {}
